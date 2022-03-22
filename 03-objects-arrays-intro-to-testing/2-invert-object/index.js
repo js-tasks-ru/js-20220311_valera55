f8a1ac6df5a88c661d6cebac2f198b4354399c2c
grigoryev <v.grigoryev@iac.spb.ru>
2022-03-22 12:14:36 +0300

    invert object done

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,18 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if(!obj){
+    return undefined;
+  }
 
+  let resObj = {};
+
+  for(let key in obj){
+
+    resObj[obj[key]] = key;
+
+    }
+
+  console.log(resObj);
+  return resObj;
 }
